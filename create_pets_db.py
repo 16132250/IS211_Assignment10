b6d3c120e4ea6c0bc5f9869a628e92ebb40345d3
16132250 <jason.curate50@spsmail.cuny.edu>
2023-04-08 13:55:08 -0400

    tweaking

--- create_pets_db.py
+++ create_pets_db.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 def create_pets_db():
 
     try:
--- music.py
+++ music.py
@@ -11,24 +11,22 @@ def create_music_db:
             con.execute('DROP TABLE IF EXISTS artists;')
 
             con.execute('''CREATE TABLE artists (
-                            artist_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            artist_name TEXT NOT NULL
+                            artist_id INTEGER PRIMARY KEY,
+                            artist_name TEXT
                             );''')
 
             con.execute('''CREATE TABLE albums (
-                            album_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            album_name TEXT NOT NULL,
-                            artist_id INTEGER NOT NULL,
-                            FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
+                            album_id INTEGER PRIMARY KEY,
+                            album_name TEXT,
+                            artist_id INTEGER
                             );''')
 
             con.execute('''CREATE TABLE songs (
-                            song_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            song_name TEXT NOT NULL,
-                            album_id INTEGER NOT NULL,
-                            track_number INTEGER NOT NULL,
-                            song_length INTEGER NOT NULL,
-                            FOREIGN KEY (album_id) REFERENCES albums(album_id)
+                            song_id INTEGER PRIMARY KEY
+                            song_name TEXT,
+                            album_id INTEGER,
+                            track_number INTEGER,
+                            song_length INTEGER
                             );''')
 
             con.commit()
